c6a95f2920de4f5f4c6c59080001631158421ef1
baileytorch <baileytorch@hotmail.com>
2026-06-11 12:20:12 -0400

    Método GET para obtener usuarios.

--- backend/server.js
+++ backend/server.js
@@ -12,13 +12,13 @@ const puerto = 3000;
 // Instanciamos las depedencias de nuetra aplicación
 aplicacion.use(cors());
 aplicacion.use(express.json());
-aplicacion.use(bodyParser.urlencoded({extended:true}));
+aplicacion.use(bodyParser.urlencoded({ extended: true }));
 aplicacion.use(bodyParser.json());
 
 // Conexion a MongoDB
 mongoose.connect('mongodb://localhost:27017/test')
-.then(() => console.log('Conexión Exitosa!') )
-.catch((excepcion) => console.log('No ha sido posible conectarse a la DB: ', excepcion));
+    .then(() => console.log('Conexión Exitosa!'))
+    .catch((excepcion) => console.log('No ha sido posible conectarse a la DB: ', excepcion));
 
 // Testtear si efectivamente la APP está corriendo en el puerto especificado
 const PORT = process.env.PORT || 3000;
@@ -32,18 +32,31 @@ const usuario = new mongoose.Schema({
     fechaNacimiento: String
 });
 
-const Usuario = mongoose.model('Usuario',usuario,'usuarios');
+const Usuario = mongoose.model('Usuario', usuario, 'usuarios');
 
-aplicacion.post('/guardarUsuario', async(req,res)=>{
-    try{
-        const{nombre,correo,contrasena,genero,fechaNacimiento} = req.body;
-        const nuevoUsuario = new Usuario({nombre,correo,contrasena,genero,fechaNacimiento});
+// Método POST para guardar datos de USUARIO
+aplicacion.post('/guardarUsuario', async (req, res) => {
+    try {
+        const { nombre, correo, contrasena, genero, fechaNacimiento } = req.body;
+        const nuevoUsuario = new Usuario({ nombre, correo, contrasena, genero, fechaNacimiento });
 
         await nuevoUsuario.save();
-        res.status(200).json({message:'Datos Ingresados Correctamente'});
+        res.status(200).json({ message: 'Datos Ingresados Correctamente' });
     }
-    catch(excepcion){
-        res.status(500).json({message:'No ha sido posible guardar los datos'});
+    catch (excepcion) {
+        res.status(500).json({ message: 'No ha sido posible guardar los datos' });
+    }
+});
+
+// Método GET para leer datos de USUARIOS
+aplicacion.get('/obtenerUsuarios', async (req, res) => {
+    try {
+        // Obtenemos una lista de objetos de tipo Usuario
+        const usuarios = await Usuario.find();
+        // En la RESPONSE (res) formateamos los usuarios como JSON y los enviamos
+        res.json(usuarios);
+    } catch (excepcion) {
+        res.status(500).json({ message: 'No ha sido posible obtener los datos. ', excepcion });
     }
 });
 
